f5a26b5989e1e40d6dee716d4c57bd42beb1b666
greg <greg.verissimo@gmail.com>
2020-05-01 14:29:34 +0200

    24. Namespaces

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-"# Dice-Roller"
+# Dice-Roller
 Project from Udacity free course : Developing Android Apps with Kotlin
 https://www.udacity.com/course/developing-android-apps-with-kotlin--ud9012
 
--- app/src/main/java/com/gregcoorp/diceroller/MainActivity.kt
+++ app/src/main/java/com/gregcoorp/diceroller/MainActivity.kt
@@ -2,15 +2,19 @@ package com.gregcoorp.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var imgvDice: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        imgvDice = imgv_dice
         val btnRoll = btn_roll
         btnRoll.text = "Let's roll"
         btnRoll.setOnClickListener {
@@ -20,7 +24,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val randomInt = Random().nextInt(6) + 1
-        tv_result.text = randomInt.toString()
+        val drawableResource = when (Random().nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        imgvDice.setImageResource(drawableResource)
     }
 }
